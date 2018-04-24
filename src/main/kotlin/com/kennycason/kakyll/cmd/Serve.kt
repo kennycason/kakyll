40b1545210a2a46fe69e701c9192946e2449158c
Chris Dempewolf <cdempewolf@simplymeasured.com>
2018-04-23 23:26:59 -0700

    Respect config's base_url property

--- src/main/kotlin/com/kennycason/kakyll/cmd/Serve.kt
+++ src/main/kotlin/com/kennycason/kakyll/cmd/Serve.kt
@@ -1,10 +1,14 @@
 package com.kennycason.kakyll.cmd
 
 import com.kennycason.kakyll.Structures
+import com.kennycason.kakyll.exception.KakyllException
 import com.kennycason.kakyll.util.FileChangeDetector
-import com.sun.net.httpserver.HttpHandler
-import com.sun.net.httpserver.HttpServer
+import com.kennycason.kakyll.view.GlobalContext
+import com.kennycason.kakyll.view.GlobalContext.config
+import com.kennycason.kakyll.view.GlobalContext.posts
 import org.apache.commons.io.FileUtils
+import org.apache.commons.lang3.StringUtils.isBlank
+import org.apache.commons.lang3.StringUtils.isNotBlank
 import org.eclipse.jetty.server.LocalConnector
 import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.server.handler.ContextHandler
@@ -16,25 +20,25 @@ import org.eclipse.jetty.server.ServerConnector
 import java.nio.file.Files
 import java.nio.file.Paths
 
-
 /**
  * Host current blog on local server
  */
 class Serve : Cmd {
+    private val DEFAULT_PORT = 8080
 
     override fun run(args: Array<String>) {
         Build().run(args)
 
-        println("Starting server on http://localhost:8080")
+        val inetSocketAddress = buildInetSocketAddress()
+        println("Starting server on http://${inetSocketAddress.hostName}:${inetSocketAddress.port}")
 
         System.setProperty("org.eclipse.jetty.LEVEL", "INFO")
 
         // start parallel thread to watch for changes
         Thread(FileChangeDetector()).start()
 
-        val server = Server()
+        val server = Server(inetSocketAddress)
         val connector = ServerConnector(server)
-        connector.port = 8080
         server.addConnector(connector)
 
         val resourceHandler = ResourceHandler()
@@ -47,6 +51,32 @@ class Serve : Cmd {
         server.setHandler(handlerList)
         server.start()
         server.join()
+    }
+
+    private fun buildInetSocketAddress(): InetSocketAddress {
+        val config = GlobalContext.config
+        val baseUrl = config.baseUrl
+
+        if (isBlank(baseUrl)) {
+            return InetSocketAddress("localhost", DEFAULT_PORT)
+        }
+
+        val pieces = config.baseUrl.split(':')
+
+        if (pieces.size == 1) {
+            return InetSocketAddress(pieces[0], DEFAULT_PORT)
+        }
+
+        if (pieces.size == 2) {
+            try {
+                return InetSocketAddress(pieces[0], Integer.valueOf(pieces[1]))
+            }
+            catch (e: NumberFormatException) {
+                throw KakyllException("Invalid port in base_url: ${baseUrl}")
+            }
+        }
+
+        throw KakyllException("Invalid base_url: ${baseUrl}")
     }
 
 }
